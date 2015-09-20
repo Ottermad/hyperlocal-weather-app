d917ab14b8891a81ecc8ec47098eee3d41d42e61
Charles Attwood Thomas <charlie.thomas@attwoodthomas.net>
2015-09-20 12:10:47 +0100

    init

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+;(function(window, document, undefined) {
+  'use strict';
+
+  window.app = {};
+
+  app.init = function() {
+    console.log('App initialised');
+  };
+
+  app.init();
+
+})(window, window.document);
